be635d76bbc18368337aa396cd0508e382df5c68
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 17:51:42 +0200

    Add project files.

---
+++ Getters_and_setters/Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters_and_setters
+{
+    internal class Movie
+    {
+        public string title;
+        public string director;
+        public string rating;
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            rating = aRating;
+        }
+        public string Rating
+        {
+            get 
+            { 
+                ReturnMessage rating; 
+            }
+            set
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }   
+
+    }
+}
---
+++ Getters_and_setters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters_and_setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Titanic", "Jim", "PG-13");
+            Movie movie1 = new Movie("Unreleased", "Evans", "PG");
+
+            Console.WriteLine(movie1.Rating);
+            Console.ReadLine();
+
+
+        }
+    }
+}
